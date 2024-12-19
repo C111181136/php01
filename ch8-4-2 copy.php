324c0c6f06b02b9cd2844596f05333877eea15dc
C111181136 <C111181136@nkust.edu.tw>
2024-12-19 11:21:18 +0800

    add

--- ch8-4-2 copy.php
+++ ch8-4-2 copy.php
@@ -1,7 +1,6 @@
 <?php
-$id =$_GET["id"];
+$id = $_GET["Id"];
 $name = $_GET["Name"];     
-echo "學號: " . $id . "<br/>姓名: " . $name . "<br/>";
+
+echo "學號: " . $id . "<br/>姓名: " . $name . "<br/>"; 
 ?>
-</body>
-</html>
